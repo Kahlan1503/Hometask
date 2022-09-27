c4b6d1e01d458751d3fd7f215344a0e39f1e586e
teri888@inbox.ru <teri888@inbox.ru>
2022-09-27 18:14:56 +0300

    add S1_task_4

---
+++ S1_task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number_3 = int.Parse(Console.ReadLine());
+// Поиск максимального из этих чисел
+int max = Math.Max(number_3, Math.Max(number_1, number_2));
+Console.WriteLine($"Максимальное число : {max}");
