7765e170275930edaf705f328e916f760d1156c9
teri888@inbox.ru <teri888@inbox.ru>
2022-09-27 20:28:36 +0300

    add S3_task_23

---
+++ S3_task_23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 23.Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+string GetTripleTable(int n)
+{
+    string tripleString = "";
+    if(n<1)
+    { 
+        return "Неверное число введено ";
+    }
+    else 
+    {
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n)
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}";
+        }
+        else
+        {
+            tripleString = tripleString + $"{Math.Pow(i, 3)}, ";
+        }
+    }
+    return tripleString;
+
+}
+}
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write($"Таблица кубов чисел от 1 до {x} = {GetTripleTable(x)}");
